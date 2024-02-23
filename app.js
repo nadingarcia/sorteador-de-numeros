db4b22d9fdd517201011a5bf95f5ddbc0440e282
nadingarcia <159864236+nadingarcia@users.noreply.github.com>
2024-02-23 20:54:10 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ function reiniciar() {
     resultados = [];
     document.getElementById('de').value = '';
     document.getElementById('ate').value = '';
+    document.getElementById('numero-sorteado').innerText = 'BINGO'; 
     document.getElementById('quantidade').value = '';
     document.getElementById('resultado').innerText = '';
     document.getElementById('btn-reiniciar').disabled = true;
